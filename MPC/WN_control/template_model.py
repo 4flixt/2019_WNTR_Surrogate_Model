b4d19baf57eb99c0788ab5058e093054f76097dc
ffiedler <felix.mh.fiedler@gmail.com>
2019-11-12 12:11:10 +0100

    Adapted template model

--- MPC/WN_control/template_model.py
+++ MPC/WN_control/template_model.py
@@ -22,38 +22,84 @@
 #
 
 from casadi import *
+from casadi.tools import *
 import numpy as NP
 import core_do_mpc
+import pickle
+import pdb
+import pandas as pd
 
 from tensorflow import keras
+
+
+def dense_nn(weights, config, nn_in):
+    """
+    Forward Propagation through a Neural Network with weights and config from keras (as imported with TF 2.0+)
+
+    weights and config are lists that can be retrieved from keras models with the get_weights, get_config method on layers
+    activation can be part of the dense layer or added as an extra layer.
+
+    p_dropout is the probability that weights will be set to zero. By defaul p_dropout is zero (scalar) and will not be executed.
+    If p_dropout is supplied it must have the same length as the amount of dropout layers in the ANN.
+    """
+    k = 0
+    for layer_i in config['layers']:
+        if 'Dense' in layer_i['class_name']:
+            nn_in = nn_in@weights[k] + weights[k+1].reshape(1, -1)
+            # Two weights for every dense layer
+            k += 2
+        if 'activation' in layer_i['config'].keys():
+            if 'linear' in layer_i['config']['activation']:
+                nn_in = nn_in
+            if 'tanh' in layer_i['config']['activation']:
+                nn_in = tanh(nn_in)
+            else:
+                print('Activation not currently supported.')
+    return nn_in
 
 
 def model():
     """
     --------------------------------------------------------------------------
-    template_model: define the non-uncertain parameters
+    template_model: Load the neural network system model
     --------------------------------------------------------------------------
     """
+    nn_model_path = './model/002_man_4x80/'
+    nn_model_name = '002_man_4x80.h5'
 
-    K0_ab = 1.287e12  # K0 [h^-1]
-    K0_bc = 1.287e12  # K0 [h^-1]
-    K0_ad = 9.043e9  # K0 [l/mol.h]
-    R_gas = 8.3144621e-3  # Universal gas constant
-    E_A_ab = 9758.3*1.00  # * R_gas# [kj/mol]
-    E_A_bc = 9758.3*1.00  # * R_gas# [kj/mol]
-    E_A_ad = 8560.0*1.0  # * R_gas# [kj/mol]
-    H_R_ab = 4.2  # [kj/mol A]
-    H_R_bc = -11.0  # [kj/mol B] Exothermic
-    H_R_ad = -41.85  # [kj/mol A] Exothermic
-    Rou = 0.9342  # Density [kg/l]
-    Cp = 3.01  # Specific Heat capacity [kj/Kg.K]
-    Cp_k = 2.0  # Coolant heat capacity [kj/kg.k]
-    A_R = 0.215  # Area of reactor wall [m^2]
-    V_R = 10.01  # 0.01 # Volume of reactor [l]
-    m_k = 5.0  # Coolant mass[kg]
-    T_in = 130.0  # Temp of inflow [Celsius]
-    K_w = 4032.0  # [kj/h.m^2.K]
-    C_A0 = (5.7+4.5)/2.0*1.0  # Concentration of A in input Upper bound 5.7 lower bound 4.5 [mol/l]
+    keras_model = keras.models.load_model(nn_model_path+nn_model_name)
+    print('----------------------------------------------------')
+    print('Loaded Keras model with the following architecture:')
+    print(keras_model.summary())
+
+    weights = keras_model.get_weights()
+    config = keras_model.get_config()
+
+    with open(nn_model_path+'train_data_param_unfiltered.pkl', 'rb') as f:
+        train_data_param = pickle.load(f)
+
+    print('----------------------------------------------------')
+    print('Loaded input and output scaling for the Keras model')
+    input_scaling = train_data_param['input_scaling']
+    output_scaling = train_data_param['output_scaling']
+
+    # with open(nn_model_path+'io_scheme.pkl', 'rb') as f:
+    #     io_scheme = pickle.load(f)
+
+    # Check if Keras and casadi return the same thing:
+    # nn_in_test = np.ones((1, 46))
+    # keras_out = keras_model.predict(nn_in_test)
+    # cas_out = dense_nn(weights, config, nn_in_test)
+    # print('Difference between CasADi and Keras Neural Network evaluation (with input all ones).')
+    # print(keras_out-cas_out)
+    # pdb.set_trace()
+
+    # Load the cluster labels and the pressure factors and determine the smallest factor for each cluster.
+    # For a given mean value of the normalized cluster pressure, this will determine the smallest physical pressure in the cluster
+    cluster_labels = pd.read_json(nn_model_path+'cluster_labels_dt1h.json')
+    pressure_factor = pd.read_json(nn_model_path+'pressure_factor_dt1h.json')
+
+    jun_cl_press_fac_min = pressure_factor.groupby(cluster_labels.loc['pressure_cluster'], axis=1).min()
 
     """
     --------------------------------------------------------------------------
@@ -61,102 +107,91 @@ def model():
     --------------------------------------------------------------------------
     """
     # Define the uncertainties as CasADi symbols
-
     alpha = SX.sym("alpha")
     beta = SX.sym("beta")
-    # Define the differential states as CasADi symbols
 
-    C_a = SX.sym("C_a")  # Concentration A
-    C_b = SX.sym("C_b")  # Concentration B
-    T_R = SX.sym("T_R")  # Reactor Temprature
-    T_K = SX.sym("T_K")  # Jacket Temprature
+    # Define the differential states as CasADi symbols
+    _x = struct_symMX([
+        entry('tank_press_T3'),
+        entry('tank_press_T1'),
+        entry('tank_press_T7'),
+        entry('tank_press_T6'),
+        entry('tank_press_T5'),
+        entry('tank_press_T2'),
+        entry('tank_press_T4'),
+    ])
+
+    # Define the disturbances as CasADi symbols
+    # These are time-varying parameters that change at each step of the prediction and at each sampling time of the MPC controller.
+    _d = MX.sym('jun_cl_demand_sum', 30)
 
     # Define the algebraic states as CasADi symbols
 
     # Define the control inputs as CasADi symbols
 
-    F = SX.sym("F")  # Vdot/V_R [h^-1]
-    Q_dot = SX.sym("Q_dot")  # Q_dot second control input
-
-    # Define time-varying parameters that can change at each step of the prediction and at each sampling time of the MPC controller. For example, future weather predictions
-
-    tv_param_1 = SX.sym("tv_param_1")
-    tv_param_2 = SX.sym("tv_param_2")
+    _u = struct_symMX([
+        entry('head_pump_PU2'),
+        entry('head_pump_PU5'),
+        entry('head_pump_PU6'),
+        entry('head_pump_PU8'),
+        entry('head_pump_PU10'),
+        entry('PRValve_V1'),
+        entry('PRValve_V45'),
+        entry('PRValve_V47'),
+        entry('TCValve_V2'),
+    ])
 
     """
     --------------------------------------------------------------------------
     template_model: define algebraic and differential equations
     --------------------------------------------------------------------------
     """
-    # Define the algebraic equations
+    nn_in_sym = vertcat(_x, _u, _d)
+    nn_in_sym_scaled = nn_in_sym/input_scaling.to_numpy()
 
-    K_1 = beta * K0_ab * exp((-E_A_ab)/((T_R+273.15)))
-    K_2 = K0_bc * exp((-E_A_bc)/((T_R+273.15)))
-    K_3 = K0_ad * exp((-alpha*E_A_ad)/((T_R+273.15)))
+    nn_out_sym_scaled = dense_nn(weights, config, nn_in_sym.T)
+    nn_out_sym = nn_out_sym_scaled.T*output_scaling.to_numpy()
 
-    # Define the differential equations
+    dtank_press = nn_out_sym[:7]
+    pump_energy = nn_out_sym[7:12]
+    jun_cl_press_mean_norm = nn_out_sym[12:]  # Mean value of the normalized cluster pressures
+    jun_cl_press_min = nn_out_sym[12:]*jun_cl_press_fac_min.to_numpy().reshape(-1, 1)
 
-    dC_a = F*(C_A0 - C_a) - K_1*C_a - K_3*(C_a**2)
-    dC_b = -F*C_b + K_1*C_a - K_2*C_b
-    dT_R = ((K_1*C_a*H_R_ab + K_2*C_b*H_R_bc + K_3*(C_a**2)*H_R_ad)/(-Rou*Cp)) + F*(T_in-T_R) + (((K_w*A_R)*(T_K-T_R))/(Rou*Cp*V_R))
-    dT_K = (Q_dot + K_w*A_R*(T_R-T_K))/(m_k*Cp_k)
-
-    # Concatenate differential states, algebraic states, control inputs and right-hand-sides
-
-    _x = vertcat(C_a, C_b, T_R, T_K)
-
-    _u = vertcat(F, Q_dot)
-
-    _xdot = vertcat(dC_a, dC_b, dT_R, dT_K)
+    _xdot = _x+dtank_press
 
     _p = vertcat(alpha, beta)
 
     _z = []
 
-    _tv_p = vertcat(tv_param_1, tv_param_2)
+    _tv_p = vertcat(_d)
 
     """
     --------------------------------------------------------------------------
     template_model: initial condition and constraints
     --------------------------------------------------------------------------
     """
-    # Initial condition for the states
-    C_a_0 = 0.8  # This is the initial concentration inside the tank [mol/l]
-    C_b_0 = 0.5  # This is the controlled variable [mol/l]
-    T_R_0 = 134.14  # [C]
-    T_K_0 = 130.0  # [C]
-    x0 = NP.array([C_a_0, C_b_0, T_R_0, T_K_0])
+    x0 = _x(1)
 
     # Bounds on the states. Use "inf" for unconstrained states
-    C_a_lb = 0.1
-    C_a_ub = 2.0
-    C_b_lb = 0.1
-    C_b_ub = 2.0
-    T_R_lb = 50.0
-    T_R_ub = 180
-    T_K_lb = 50.0
-    T_K_ub = 180
-    x_lb = NP.array([C_a_lb, C_b_lb, T_R_lb, T_K_lb])
-    x_ub = NP.array([C_a_ub, C_b_ub, T_R_ub, T_K_ub])
+    x_lb = _x(0)
+    x_ub = _x(20)
 
     # Bounds on the control inputs. Use "inf" for unconstrained inputs
-    F_lb = 5.0
-    F_ub = +100.0
-    Q_dot_lb = -8500.0
-    Q_dot_ub = 0.0
-    u_lb = NP.array([F_lb, Q_dot_lb])
-    u_ub = NP.array([F_ub, Q_dot_ub])
-    u0 = NP.array([30.0, -6000.0])
+    u_lb = _u(0)
+    u_ub = _u(np.inf)
+    u0 = _u(0)
 
     # Scaling factors for the states and control inputs. Important if the system is ill-conditioned
-    x_scaling = NP.array([1.0, 1.0, 1.0, 1.0])
-    u_scaling = NP.array([1.0, 1.0])
+    x_scaling = _x(1)
+    u_scaling = _u(1)
 
     # Other possibly nonlinear constraints in the form cons(x,u,p) <= cons_ub
     # Define the expresion of the constraint (leave it empty if not necessary)
-    cons = vertcat([])
+    cons = vertcat(50-jun_cl_press_min,
+                   -pump_energy
+                   )
     # Define the lower and upper bounds of the constraint (leave it empty if not necessary)
-    cons_ub = NP.array([])
+    cons_ub = NP.array([np.zeros((35, 1))])
 
     # Activate if the nonlinear constraints should be implemented as soft constraints
     soft_constraint = 0
@@ -178,13 +213,18 @@ def model():
     """
     # Define the cost function
     # Lagrange term
-    lterm = 1e4*((C_b - 0.9)**2 + (C_a - 1.1)**2)
-    #lterm =  - C_b
+    lterm = pump_energy
+    # lterm =  - C_b
     # Mayer term
-    mterm = 1e4*((C_b - 0.9)**2 + (C_a - 1.1)**2)
-    #mterm =  - C_b
+    mterm = 0
+    # mterm =  - C_b
     # Penalty term for the control movements
-    rterm = NP.array([0.0, 0.0])
+    rterm = _u(0)
+
+    # For compliance with do_mpc:
+    # TODO: Check if necessary.
+    # _x = vertcat(_x)
+    # _u = vertcat(_u)
 
     """
     --------------------------------------------------------------------------
